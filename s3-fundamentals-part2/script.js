b789887f55ae6f15d473f953eb5ed0988cd6b05a
Wenwen Kong <wanzi0411@gmail.com>
2022-12-11 22:22:25 -0800

    up to lecture #40

--- s3-fundamentals-part2/script.js
+++ s3-fundamentals-part2/script.js
@@ -132,4 +132,204 @@ const yearsUntilRetirement2 = (birthYear, firstName) => {
 }
 console.log(yearsUntilRetirement2(1991, 'Jonas'));
 
+// -----------------------------------------------
+// Lecture 36. Functions Calling Other Functions
+// -----------------------------------------------
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of 
+    apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+// -----------------------------------------------
+// Lecture 37. Reviewing Functions 
+// -----------------------------------------------
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+
+const yearsUntilRetirement3 = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        return retirement;
+        console.log(`${firstName} retires in ${retirement} years`);
+    } else {
+        return -1;
+        console.log(`${firstName} has already retired 🎉`);
+    }
+}
+
+console.log(yearsUntilRetirement3(1991, 'Jonas'));
+console.log(yearsUntilRetirement3(1950, 'Mike'));
+
+/*
+
+The console.log from the above function is ignored and not get executed,
+because the return statement immediately exits / returns the function.
+So there is no chance the code pieces after the return statement is even reached.
+
+Can use Option + up arrow to move the console.log code up to execute it.
+
+*/
+
+/*
+
+Function Review: 3 different function types
+
+- Function declaration: can be used before it's declared
+
+- Function expression: essentially a function value stored in a variable
+
+- Arrow function: great for quick one-line functions. Has no 'this' keyword (more later ..)
+
+*/
+
+// -----------------------------------------------
+// Lecture 38. coding challenge # 1
+// -----------------------------------------------
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const avgDolphins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win (${avgKoalas}) vs. ${avgDolphins}`);
+    } else {
+        console.log('No team wins...');
+    }
+}
+
+checkWinner(avgDolphins, avgKoalas);
+
+// -----------------------------------------------
+// Lecture 39. Introduction to Arrays
+// -----------------------------------------------
+/*
+The two most important data structures in JavaScript are arrays and objects.
+*/
+
+// Literal syntax 
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Or, create array using new Array()
+const years = new Array(1991, 1984, 2008, 2020);
+
+// Arrays are zero-based 
+console.log(friends[0]);
+
+// .length to get the amount of elements in the array
+console.log(friends.length);
+
+// to get the last element
+console.log(friends[friends.length - 1]);
+
+// mutate array's element
+friends[2] = 'Jay'; // now friends becomes ['Michael', 'Steven', 'Jay']
+
+/*
+Array elements are mutatable, but we cannot change the whole array like this:
+friends = ['Bob', 'Alice']
+*/
+
+const firstName = 'Jonas'
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+
+console.log(jonas);
+
+/*
+Exercise
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+we can not do calcAge(years2)
+
+years2 + 10 gives us '1990, 1967, 2002, 2010, 201810' because + sign converts the array elements to string
+
+years2 - 10 gives us NaN
+
+===> we cannot directly do operation with arrays
+
+*/
+
+const years2 = [1990, 1967, 2002, 2010, 2018];
+
+const ages = [calcAge(years2[0]), calcAge(years2[1]), calcAge(years2[years2.length - 1])];
+
+// -----------------------------------------------
+// Lecture 40. Basic Array Operations (Methods)
+// -----------------------------------------------
+
+/*
+.push method adds elements to the end of an array
+*/
+
+const newLength = friends.push('Jay');
+console.log(newLength);
+
+/*
+.unshift method adds elements to the beginning of an array
+*/
+
+friends.unshift('John');
+console.log(friends);
+
+/*
+.pop method removes the last element 
+*/
+const popped = friends.pop();
+console.log(popped); // element that is removed
+console.log(friends);
+
+/*
+.shift() remove the first element
+*/
+
+friends.shift(); // 
+console.log(friends);
+
+/*
+.indexOf method tells the index of an element 
+
+if we try an element that does not exist in the array, we will get -1
+*/
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob')); // -1
+
+/*
+Added in ES6
+.includes returns true or false of whether an element is in the array
+
+*/
+
+friends.push(23);
+console.log(friends.includes('Steven')); // true
+console.log(friends.includes('Bob')); // false
+console.log(friends.includes('23')); // false --> does not do type coercion 
+console.log(friends.includes(23)); // true
+
+// .includes can be used in conditional statement 
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
+}
 
