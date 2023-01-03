91a8191b5ee8f5bd720447f283813406d093928d
Wenwen Kong <wanzi0411@gmail.com>
2023-01-02 22:07:46 -0800

    project #3: pig-game

---
+++ s7-pig-game/script.js
@@ -0,0 +1,116 @@
+'use strict';
+
+// -------------------------------------------
+// Lecture 82. Project #3: Pig Game
+// -------------------------------------------
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+// Two ways to select element based on ID
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// utility function
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0]; // Final scores for two players
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+// -------------------------------------------
+// Lecture 83. Rolling the dice
+// -------------------------------------------
+
+// Rolling dice functionality
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // switch to the next player and set current score of current active player to zero
+      // Switch the background using the .toggle method
+      switchPlayer();
+    }
+  }
+});
+
+// -------------------------------------------
+// Lecture 84. Switching the Active Player
+// -------------------------------------------
+// See above
+
+// -------------------------------------------
+// Lecture 85. Holding current score
+// -------------------------------------------
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // If not, switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+// -------------------------------------------
+// Lecture 86. Resetting the game
+// -------------------------------------------
+btnNew.addEventListener('click', init);
